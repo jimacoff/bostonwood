3d0e06ecfeb81c5078cf979e78a0103266bd6378
Nathan W <nate01776@gmail.com>
2016-11-05 20:01:53 -0400

    edit product form added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,22 @@ class ProductsController < ApplicationController
   def update
     @category = Category.find(params[:category_id])
     @product = @category.products.find(params[:id])
-    @product.image = params[:product][:image]
-    @product.save
+
+    @product.update_attributes(product_params)
+    if !params[:product][:image].nil?
+      @product.image = params[:product][:image]
+    end
+    if @product.save
+      redirect_to root_path
+    else
+      render action: 'edit'
+    end
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :blurb, :description, :build_material, :image)
+  end
+
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,10 +5,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   process :tags => ['post_picture']
 
   version :standard do
-    process :resize_to_fill => [200, 200, :north]
+    process :resize_to_fill => [400, 400, :north]
   end
 
   version :thumbnail do
-    resize_to_fit(50, 50)
+    resize_to_fit(200, 200)
   end
 end
