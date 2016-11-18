781133f980f4b2336cd0c5db5e404d5da3e708a3
Nathan W <nate01776@gmail.com>
2016-11-17 19:13:26 -0500

    added new homepage layout

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 class SiteController < ApplicationController
   def index
+    @pictures = []
+    Dir.foreach('./public/images') do |picture|
+      next if picture == '.' or picture == '..'
+      @pictures << picture
+    end
   end
 end
--- db/migrate/20161107205210_add_pricing_table.rb
+++ db/migrate/20161107205210_add_pricing_table.rb
@@ -1,6 +1,6 @@
 class AddPricingTable < ActiveRecord::Migration[5.0]
   def change
-    create_table :pricing do |t|
+    create_table :pricings do |t|
       t.belongs_to :product, null: false
       t.jsonb :data
     end
