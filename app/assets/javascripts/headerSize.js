170473f09deba6a874a93532150a3483a13e7b0f
Nathan W <nate01776@gmail.com>
2017-01-12 16:10:07 -0500

    added hours to header, disappear on shrink

--- app/assets/javascripts/headerSize.js
+++ app/assets/javascripts/headerSize.js
@@ -3,14 +3,17 @@ function init() {
     var distanceY = window.pageYOffset || document.documentElement.scrollTop,
         shrinkOn = 50,
         header = document.getElementsByClassName("top-header");
+        hours = document.getElementsByClassName("hours")
         nav = document.getElementsByClassName("sidebar")
     if (distanceY > shrinkOn) {
       header[0].className += " smaller";
       nav[0].className += " smaller";
+      hours[0].className += " smaller"
     } else {
       if (header[0].classList.contains("smaller")) {
         header[0].className = "top-header"
         nav[0].className = "sidebar"
+        hours[0].className = "hours"
       }
     }
   });
