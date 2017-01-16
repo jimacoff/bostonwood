bb5bb3977644affe9fe742168c19a1df63197596
Nathan W <nate01776@gmail.com>
2017-01-16 16:26:16 -0500

    pricing not applied to correct tables

--- app/assets/javascripts/fin_table_creation.js
+++ app/assets/javascripts/fin_table_creation.js
@@ -29,7 +29,7 @@ $(function() {
           priceRowStart = priceRowStart + "<td>" + heights[n].split("_").join(" ") + "</td>";
 
           for(x = 0; x < prices.length; x++) {
-            priceRowStart = priceRowStart + "<td>" + (prices[x] * .75) + ".99</td>";
+            priceRowStart = priceRowStart + "<td>" + prices[x] + ".99</td>";
           }
           table.append(priceRowStart);
       }};
