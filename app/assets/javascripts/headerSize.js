0785a66e8de75c048ecf8095c44afb33a5de1d6d
Nathan W <nate01776@gmail.com>
2017-01-18 16:27:59 -0500

    flag image added

--- app/assets/javascripts/headerSize.js
+++ app/assets/javascripts/headerSize.js
@@ -2,18 +2,24 @@ function init() {
   window.addEventListener('scroll', function(event) {
     var distanceY = window.pageYOffset || document.documentElement.scrollTop,
         shrinkOn = 50,
-        header = document.getElementsByClassName("top-header");
+        bar = document.getElementsByClassName("top-header")
+        header = document.getElementsByClassName("header-center");
         hours = document.getElementsByClassName("hours")
         nav = document.getElementsByClassName("sidebar")
+        flag = document.getElementsByClassName("flag-container")
     if (distanceY > shrinkOn) {
-      header[0].className = "top-header smaller";
+      bar[0].className = "top-header smaller"
+      header[0].className = "header-center smaller";
       nav[0].className = "sidebar smaller";
       hours[0].className = "hours smaller"
+      flag[0].className = "flag-container smaller"
     } else {
       if (header[0].classList.contains("smaller")) {
-        header[0].className = "top-header"
+        bar[0].className = "top-header"
+        header[0].className = "header-center"
         nav[0].className = "sidebar"
         hours[0].className = "hours"
+        flag[0].className = "flag-container"
       }
     }
   });
