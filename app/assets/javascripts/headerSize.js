bf1e7ef9bb99d82263731fa3d4d69ded05dd2afe
Nathan W <nate01776@gmail.com>
2017-01-18 15:34:15 -0500

    fixing my mistake on last push

--- app/assets/javascripts/headerSize.js
+++ app/assets/javascripts/headerSize.js
@@ -6,9 +6,9 @@ function init() {
         hours = document.getElementsByClassName("hours")
         nav = document.getElementsByClassName("sidebar")
     if (distanceY > shrinkOn) {
-      header[0].className = "sidebar smaller";
+      header[0].className = "top-header smaller";
       nav[0].className = "sidebar smaller";
-      hours[0].className = "sidebar smaller"
+      hours[0].className = "hours smaller"
     } else {
       if (header[0].classList.contains("smaller")) {
         header[0].className = "top-header"
