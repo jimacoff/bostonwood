001867c5290b27b4999d59d4bc35634415c2ceb8
Nathan W <nate01776@gmail.com>
2017-01-25 14:50:37 -0500

    further mobile changes for sidebar

--- app/assets/javascripts/sidebar_animation.js
+++ app/assets/javascripts/sidebar_animation.js
@@ -3,15 +3,26 @@ function openNav() {
   var main = document.getElementsByClassName("main")
   var hamburger = document.getElementsByClassName("open-button")
   var close = document.getElementsByClassName("close-button")
-  main[0].style.marginLeft = "250px";
-  main[0].style.width = "80%";
-  sidebar[0].style.width = "250px";
-  sidebar[0].style.paddingLeft = "25px";
-  sidebar[0].style.paddingRight = "25px";
-  hamburger[0].innerHTML = "<i class='fa fa-times' aria-hidden='true'></i>"
-  hamburger[0].onclick = closeNav
-  close[0].style.opacity = 1;
-  hamburger[0].style.opacity = 0;
+  if (window.innerWidth > 600) {
+    main[0].style.marginLeft = "250px";
+    main[0].style.width = "80%";
+    sidebar[0].style.width = "250px";
+    sidebar[0].style.paddingLeft = "25px";
+    sidebar[0].style.paddingRight = "25px";
+    hamburger[0].innerHTML = "<i class='fa fa-times' aria-hidden='true'></i>"
+    hamburger[0].onclick = closeNav
+    close[0].style.opacity = 1;
+    hamburger[0].style.opacity = 0;
+  } else {
+    sidebar[0].style.width = "250px";
+    sidebar[0].style.paddingLeft = "25px";
+    sidebar[0].style.paddingRight = "25px";
+    hamburger[0].innerHTML = "<i class='fa fa-times' aria-hidden='true'></i>"
+    hamburger[0].onclick = closeNav
+    close[0].style.opacity = 1;
+    hamburger[0].style.opacity = 0;
+  }
+
 }
 
 function closeNav() {
@@ -19,13 +30,25 @@ function closeNav() {
   var main = document.getElementsByClassName("main")
   var hamburger = document.getElementsByClassName("open-button")
   var close = document.getElementsByClassName("close-button")
-  main[0].style.marginLeft = "0px";
-  main[0].style.width = "100%";
-  sidebar[0].style.width = "0px";
-  sidebar[0].style.paddingLeft = "0px";
-  sidebar[0].style.paddingRight = "0px";
-  hamburger[0].innerHTML = "<i class='fa fa-bars' aria-hidden='true'></i>"
-  hamburger[0].onclick = openNav;
-  close[0].style.opacity = 0;
-  hamburger[0].style.opacity = 1;
+
+  if (window.innerWidth > 600) {
+    sidebar[0].style.width = "0px";
+    sidebar[0].style.paddingLeft = "0px";
+    sidebar[0].style.paddingRight = "0px";
+    hamburger[0].innerHTML = "<i class='fa fa-bars' aria-hidden='true'></i>"
+    hamburger[0].onclick = openNav;
+    close[0].style.opacity = 0;
+    hamburger[0].style.opacity = 1;
+  } else {
+    main[0].style.marginLeft = "0px";
+    main[0].style.width = "100%";
+    sidebar[0].style.width = "0px";
+    sidebar[0].style.paddingLeft = "0px";
+    sidebar[0].style.paddingRight = "0px";
+    hamburger[0].innerHTML = "<i class='fa fa-bars' aria-hidden='true'></i>"
+    hamburger[0].onclick = openNav;
+    close[0].style.opacity = 0;
+    hamburger[0].style.opacity = 1;
+  }
+
 }
