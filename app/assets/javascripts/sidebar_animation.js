99e23fda79999bd24bd3b74aeb2ba5f154994d5b
Nathan W <nate01776@gmail.com>
2017-01-25 14:13:31 -0500

    show open/close only, moved close inside sidebar

--- app/assets/javascripts/sidebar_animation.js
+++ app/assets/javascripts/sidebar_animation.js
@@ -2,6 +2,7 @@ function openNav() {
   var sidebar = document.getElementsByClassName("sidebar");
   var main = document.getElementsByClassName("main")
   var hamburger = document.getElementsByClassName("open-button")
+  var close = document.getElementsByClassName("close-button")
   main[0].style.marginLeft = "250px";
   main[0].style.width = "80%";
   sidebar[0].style.width = "250px";
@@ -9,17 +10,22 @@ function openNav() {
   sidebar[0].style.paddingRight = "25px";
   hamburger[0].innerHTML = "<i class='fa fa-times' aria-hidden='true'></i>"
   hamburger[0].onclick = closeNav
+  close[0].style.opacity = 1;
+  hamburger[0].style.opacity = 0;
 }
 
 function closeNav() {
   var sidebar = document.getElementsByClassName("sidebar");
   var main = document.getElementsByClassName("main")
   var hamburger = document.getElementsByClassName("open-button")
+  var close = document.getElementsByClassName("close-button")
   main[0].style.marginLeft = "0px";
   main[0].style.width = "100%";
   sidebar[0].style.width = "0px";
   sidebar[0].style.paddingLeft = "0px";
   sidebar[0].style.paddingRight = "0px";
   hamburger[0].innerHTML = "<i class='fa fa-bars' aria-hidden='true'></i>"
-  hamburger[0].onclick = openNav
+  hamburger[0].onclick = openNav;
+  close[0].style.opacity = 0;
+  hamburger[0].style.opacity = 1;
 }
