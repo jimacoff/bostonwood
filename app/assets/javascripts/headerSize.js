62b9482610932b8d0e18ef86ecec214b1d344e3d
Nathan W <nate01776@gmail.com>
2017-01-31 11:39:52 -0500

    removed scaling header, added box shadow

--- app/assets/javascripts/headerSize.js
+++ app/assets/javascripts/headerSize.js
@@ -1,31 +1,31 @@
-function init() {
-  window.addEventListener('scroll', function(event) {
-    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-        shrinkOn = 50,
-        bar = document.getElementsByClassName("top-header")
-        header = document.getElementsByClassName("header-center");
-        hours = document.getElementsByClassName("hours")
-        nav = document.getElementsByClassName("sidebar")
-        flag = document.getElementsByClassName("flag-container")
-        flagImage = document.getElementsByClassName("flag-image")
-    if (distanceY > shrinkOn) {
-      bar[0].className = "top-header smaller"
-      header[0].className = "header-center smaller";
-      nav[0].className = "sidebar smaller";
-      hours[0].className = "hours smaller"
-      flag[0].className = "flag-container smaller"
-      flagImage[0].className = "flag-image smaller"
-    } else {
-      if (header[0].classList.contains("smaller")) {
-        bar[0].className = "top-header"
-        header[0].className = "header-center"
-        nav[0].className = "sidebar"
-        hours[0].className = "hours"
-        flag[0].className = "flag-container"
-        flagImage[0].className = "flag-image"
-      }
-    }
-  });
-}
+// function init() {
+//   window.addEventListener('scroll', function(event) {
+//     var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+//         shrinkOn = 50,
+//         bar = document.getElementsByClassName("top-header")
+//         header = document.getElementsByClassName("header-center");
+//         hours = document.getElementsByClassName("hours")
+//         nav = document.getElementsByClassName("sidebar")
+//         flag = document.getElementsByClassName("flag-container")
+//         flagImage = document.getElementsByClassName("flag-image")
+//     if (distanceY > shrinkOn) {
+//       bar[0].className = "top-header smaller"
+//       header[0].className = "header-center smaller";
+//       nav[0].className = "sidebar smaller";
+//       hours[0].className = "hours smaller"
+//       flag[0].className = "flag-container smaller"
+//       flagImage[0].className = "flag-image smaller"
+//     } else {
+//       if (header[0].classList.contains("smaller")) {
+//         bar[0].className = "top-header"
+//         header[0].className = "header-center"
+//         nav[0].className = "sidebar"
+//         hours[0].className = "hours"
+//         flag[0].className = "flag-container"
+//         flagImage[0].className = "flag-image"
+//       }
+//     }
+//   });
+// }
 
 window.onload = init()
