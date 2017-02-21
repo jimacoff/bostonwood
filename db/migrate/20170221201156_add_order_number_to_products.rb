214609a00e3a46ce51b83bd8c6f820aa6093c995
Nathan W <nate01776@gmail.com>
2017-02-21 15:16:24 -0500

    migration run and successfully added to DB

--- db/migrate/20170221201156_add_order_number_to_products.rb
+++ db/migrate/20170221201156_add_order_number_to_products.rb
@@ -1,4 +1,9 @@
 class AddOrderNumberToProducts < ActiveRecord::Migration[5.0]
-  def change
+  def up
+    add_column :products, :order_num, :integer
+  end
+
+  def down
+    remove_column :products, :order_num
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107205210) do
+ActiveRecord::Schema.define(version: 20170221201156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20161107205210) do
     t.integer "category_id",    null: false
     t.integer "builder_id",     null: false
     t.string  "image"
+    t.integer "order_num"
     t.index ["builder_id"], name: "index_products_on_builder_id", using: :btree
     t.index ["category_id"], name: "index_products_on_category_id", using: :btree
   end
