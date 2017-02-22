b80a7d4b368d3537335d638e84f19dff6e45bf3f
Nathan W <nate01776@gmail.com>
2017-02-22 11:03:13 -0500

    changing price grid phrasing

--- app/assets/javascripts/unf_table_creation.js
+++ app/assets/javascripts/unf_table_creation.js
@@ -16,7 +16,7 @@ $(function() {
         table.append(row)
 
       } else {
-      var header = $("<thead><tr><th width='150' colspan=" + (widths.length + 1) + " style='text-align:center;'>Unfinished " + product.build_material + "</th></tr></thead>");
+      var header = $("<thead><tr><th width='150' colspan=" + (widths.length + 1) + " style='text-align:center;'>" + product.build_material + " - unfinished</th></tr></thead>");
       table.append(header);
       // add widths to top of table
       var rowWidthFunction = function(inputArray) {
@@ -51,7 +51,7 @@ $(function() {
     }}
 
     if (builder.name === "Evergreen" && widths[0] === "Size") {
-        var header = $("<thead><tr><th width='150' colspan=" + (widths.length + 1) + " style='text-align:center;'>Unfinished " + product.build_material + "</th></tr></thead>");
+        var header = $("<thead><tr><th width='150' colspan=" + (widths.length + 1) + " style='text-align:center;'>" + product.build_material + " - unfinished</th></tr></thead>");
         table.append(header);
         // add widths to top of table
         var rowWidthFunction = function(inputArray) {
@@ -86,7 +86,7 @@ $(function() {
 
       if (builder.name !== "Evergreen" && widths[0] !== "Size") {
           // add header to table
-          var header = $("<thead><tr><th width='150' colspan=" + (widths.length + 2) + " style='text-align:center;'>Unfinished " + product.build_material + "</th></tr></thead>");
+          var header = $("<thead><tr><th width='150' colspan=" + (widths.length + 2) + " style='text-align:center;'>" + product.build_material + " - unfinished</th></tr></thead>");
           table.append(header);
           //add widths name to table
           var widthsTitle = $("<tr><td colspan=" + (widths.length + 2) + " style='text-align:center;' class='width-name'><b>width</b></td></tr>")
