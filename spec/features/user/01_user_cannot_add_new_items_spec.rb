1f59e88ad73fc5ec500cf81e8dc680e27b99b568
Nathan W <nate01776@gmail.com>
2017-04-06 20:29:40 -0400

    working on user product tests

--- spec/features/user/01_user_cannot_add_new_items_spec.rb
+++ spec/features/user/01_user_cannot_add_new_items_spec.rb
@@ -29,10 +29,15 @@ describe 'User should NOT be able to' do
       expect(page).to_not have_field('builder_state')
       expect(page).to_not have_button('Add New Builder')
     end
-    #
-    # scenario 'products' do
-    #   visit '/categories/new'
-    #   expect(page).to_not have_content('LOCATIONS')
-    # end
+
+    scenario 'products' do
+      visit '/categories/' + category.name + '/products/new'
+      expect(page).to_not have_content('New Product - ' + category.name)
+      expect(page).to_not have_field('product_name')
+      expect(page).to_not have_field('product_blurb')
+      expect(page).to_not have_field('product_description')
+      expect(page).to_not have_field('product_build_material')
+      expect(page).to_not have_button('Add New Product')
+    end
   end
 end
